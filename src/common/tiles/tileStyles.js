064ac7975a0c9599d0f0f1914f2ec722e765b3bb
KalinaJakubowska <witillie@gmail.com>
2020-12-04 11:00:50 +0100

    add tileStyles

---
+++ src/common/tiles/tileStyles.js
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+
+export const TileWrapper = styled.div`
+  background: #ffffff;
+  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
+    0px 16px 58px rgba(9, 10, 51, 0.03);
+  border-radius: 4px;
+`;
