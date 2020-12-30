9047d9aa32923e25c00d7f89e4eca51030aced02
KalinaJakubowska <witillie@gmail.com>
2020-12-30 23:54:10 +0100

    add colors list out of theme object

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,17 @@
+const colors = {
+  mineShaft: "#252525",
+  scienceBlue: "#0366D6",
+  slateGray: "#6E7E91",
+  white: "#ffffff",
+  whiteLiliac: "#FBFBFE",
+};
+
 export const theme = {
   color: {
-    mineShaft: "#252525",
-    scienceBlue: "#0366D6",
-    slateGray: "#6E7E91",
-    white: "#ffffff",
-    whiteLiliac: "#FBFBFE",
+    title: colors.mineShaft,
+    button: colors.scienceBlue,
+    text: colors.slateGray,
+    tileBackground: colors.white,
+    background: colors.whiteLiliac,
   },
 };
