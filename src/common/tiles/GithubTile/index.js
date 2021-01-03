b55cabea355476dd8bacc397fa374ba13b2da06c
KalinaJakubowska <witillie@gmail.com>
2021-01-04 00:31:15 +0100

    add Github details with styles

--- src/common/tiles/GithubTile/index.js
+++ src/common/tiles/GithubTile/index.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { GithubTileWrapper, GithubContainer } from "./styled";
+import {
+  GithubTileWrapper,
+  GithubContainer,
+  GithubTitle,
+  GithubDescription,
+  GithubLink,
+  GithubLinkWrapper,
+} from "./styled";
 import { selectGithubProjects, selectLoading } from "./../../../githubSlice";
 import { useSelector } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
@@ -12,7 +19,20 @@ const Github = () => {
     <GithubContainer>
       {!isLoading &&
         githubProjects.map((project) => (
-          <GithubTileWrapper key={nanoid()}>{project.name}</GithubTileWrapper>
+          <GithubTileWrapper key={nanoid()}>
+            <GithubTitle>{project.name}</GithubTitle>
+            <GithubDescription>{project.description}</GithubDescription>
+            {project.homepage && (
+              <GithubLinkWrapper>
+                Demo: <GithubLink>{project.homepage}</GithubLink>
+              </GithubLinkWrapper>
+            )}
+            {project.html_url && (
+              <GithubLinkWrapper>
+                Code: <GithubLink>{project.html_url}</GithubLink>
+              </GithubLinkWrapper>
+            )}
+          </GithubTileWrapper>
         ))}
     </GithubContainer>
   );
--- src/common/tiles/GithubTile/styled.js
+++ src/common/tiles/GithubTile/styled.js
@@ -1,11 +1,37 @@
 import styled from "styled-components";
 import { TileWrapper } from "../tileStyles";
 
-export const GithubTileWrapper = styled(TileWrapper)`
-  border: 6px solid rgba(209, 213, 218, 0.3);
-`;
-
 export const GithubContainer = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 32px;
+`;
+
+export const GithubTileWrapper = styled(TileWrapper)`
+  border: 6px solid rgba(209, 213, 218, 0.3);
+  padding: 56px;
+`;
+
+export const GithubTitle = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 1.4;
+  color: ${({ theme }) => theme.color.button};
+`;
+
+export const GithubDescription = styled.p`
+  line-height: 1.4;
+  color: ${({ theme }) => theme.color.text};
+  margin: 24px 0;
+`;
+
+export const GithubLinkWrapper = styled.span`
+  color: ${({ theme }) => theme.color.text};
+  display: block;
+  margin-top: 8px;
+`;
+
+export const GithubLink = styled.a`
+  display: inline;
+  color: ${({ theme }) => theme.color.button};
 `;
