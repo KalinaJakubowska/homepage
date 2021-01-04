14b447258c50f261443e831411b9cf46985bb715
KalinaJakubowska <witillie@gmail.com>
2021-01-04 17:55:31 +0100

    add GithubHeader for Gihub component

--- src/features/Github/index.js
+++ src/features/Github/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
-import { GithubContainer } from "./styled";
+import {
+  GithubTilesContainer,
+  GithubHeader,
+  Title,
+  Description,
+} from "./styled";
 import { selectGithubProjects } from "./../../githubSlice";
 import GithubTile from "../../common/tiles/GithubTile";
 import { selectError, selectLoading } from "../../githubSlice";
 import Checker from "../../common/Checker";
+import { ReactComponent as GithubLogo } from "./../../assets/logos/githubLogo.svg";
+import { theme } from "../../theme";
 
 const Github = () => {
   const githubProjects = useSelector(selectGithubProjects);
@@ -14,7 +21,12 @@ const Github = () => {
 
   return (
     <Checker isLoading={isLoading} isError={isError}>
-      <GithubContainer>
+      <GithubHeader>
+        <GithubLogo fill={theme.color.button}/>
+        <Title>Portfolio</Title>
+        <Description>My recent projects</Description>
+      </GithubHeader>
+      <GithubTilesContainer>
         {githubProjects.map((project) => (
           <GithubTile
             name={project.name}
@@ -24,7 +36,7 @@ const Github = () => {
             key={nanoid()}
           />
         ))}
-      </GithubContainer>
+      </GithubTilesContainer>
     </Checker>
   );
 };
--- src/features/Github/styled.js
+++ src/features/Github/styled.js
@@ -1,7 +1,29 @@
 import styled from "styled-components";
 
-export const GithubContainer = styled.div`
+export const GithubHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 72px;
+  color: ${({ theme }) => theme.color.title};
+`;
+
+export const GithubTilesContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 32px;
+  margin-top: 24px;
+`;
+
+export const Title = styled.p`
+  font-size: 30px;
+  line-height: 1.2;
+  font-weight: 900;
+  margin: 13px 0 0 0;
+`;
+
+export const Description = styled.p`
+  font-size: 20px;
+  line-height: 1.4;
+  margin: 8px 0 0 0;
 `;
