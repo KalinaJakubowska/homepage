315acca24c284176e824d08cdf861e089da5c195
KalinaJakubowska <witillie@gmail.com>
2021-01-04 22:37:55 +0100

    add Error styles

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { Text } from "./styled";
+import { ErrorWrapper, Alert, Explanation } from "./styled";
+import { ReactComponent as DangerIcon } from "./../../assets/icons/danger.svg";
+import { Button } from "./../../common/Button";
 
-const Error = () => <Text>Something went wrong...</Text>;
+const Error = () => (
+  <ErrorWrapper>
+    <DangerIcon />
+    <Alert>Ooops! Something went wrong...</Alert>
+    <Explanation>
+      Sorry, failed to load Github projects. You can check them directly on
+      Github.
+    </Explanation>
+    <Button>Go to Github</Button>
+  </ErrorWrapper>
+);
 
 export default Error;
--- src/common/Error/styled.js
+++ src/common/Error/styled.js
@@ -1,5 +1,25 @@
 import styled from "styled-components";
 
-export const Text = styled.p`
+export const ErrorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 430px;
+  margin: auto;
+  text-align: center;
+`;
+
+export const Alert = styled.p`
   color: ${({ theme }) => theme.color.title};
+  font-size: 24px;
+  line-height: 1.2;
+  font-weight: bold;
+  margin: 24px 0 0;
+`;
+
+export const Explanation = styled.p`
+  color: ${({ theme }) => theme.color.title};
+  margin: 32px 0 0;
+  font-size: 20px;
+  line-height: 1.4;
 `;
