e4a265b21aa4ec2eb452fd54bbfff03bc2d73c73
KalinaJakubowska <witillie@gmail.com>
2021-01-04 16:02:08 +0100

    add github api error handling

--- src/features/Github/index.js
+++ src/features/Github/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { GithubContainer } from "./styled";
-import { selectGithubProjects, selectLoading } from "./../../githubSlice";
+import {
+  selectGithubProjects,
+  selectLoading,
+  selectError,
+} from "./../../githubSlice";
 import { useSelector } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
 import GithubTile from "../../common/tiles/GithubTile";
+import Error from "./../../common/Error";
+import Loading from "./../../common/Loading";
 
 const Github = () => {
   const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
   const githubProjects = useSelector(selectGithubProjects);
 
   return (
     <GithubContainer>
-      {!isLoading &&
+      {isLoading ? (
+        <Loading />
+      ) : isError ? (
+        <Error />
+      ) : (
         githubProjects.map((project) => (
           <GithubTile
             name={project.name}
@@ -20,7 +31,8 @@ const Github = () => {
             html_url={project.html_url}
             key={nanoid()}
           />
-        ))}
+        ))
+      )}
     </GithubContainer>
   );
 };
