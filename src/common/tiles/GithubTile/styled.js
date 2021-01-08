cd799e6df701a2a9185251d4dc75605e000164e3
KalinaJakubowska <witillie@gmail.com>
2021-01-08 16:56:29 +0100

    change colors for dark theme

--- src/common/tiles/GithubTile/styled.js
+++ src/common/tiles/GithubTile/styled.js
@@ -17,7 +17,7 @@ export const GithubTitle = styled.div`
   font-weight: bold;
   font-size: 24px;
   line-height: 1.4;
-  color: ${({ theme }) => theme.color.button};
+  color: ${({ theme }) => theme.color.githubTileTitle};
   margin-bottom: 24px;
 `;
 
--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,8 @@ const colors = {
   slateGray: "#6E7E91",
   white: "#ffffff",
   whiteLiliac: "#FBFBFE",
+  mercury: "#E5E5E5",
+  dodgerBlue: "#2188FF",
 };
 
 export const theme = {
@@ -14,6 +16,24 @@ export const theme = {
     tileBackground: colors.white,
     background: colors.whiteLiliac,
     githubTileHover: "rgba(3, 102, 214, 0.2)",
+    themeSwitchBackground: colors.mercury,
+    themeSwitchBorder: colors.slateGray,
+    githubTileTitle: colors.scienceBlue,
+  },
+  colors,
+};
+
+export const darkTheme = {
+  color: {
+    title: colors.white,
+    button: colors.dodgerBlue,
+    text: colors.white,
+    tileBackground: "rgba(54, 54, 54, 0.72)",
+    background: colors.mineShaft,
+    githubTileHover: "rgba(3, 102, 214, 0.5)",
+    themeSwitchBackground: "rgba(54, 54, 54, 0.72)",
+    themeSwitchBorder: colors.white,
+    githubTileTitle: colors.white,
   },
   colors,
 };
