e45019ccb908feebad523101916fbc7768660915
KalinaJakubowska <witillie@gmail.com>
2021-01-28 15:09:02 +0100

    change HeaderWrapper to header instead of div

--- src/features/Header/styled.js
+++ src/features/Header/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const HeaderWrapper = styled.div`
+export const HeaderWrapper = styled.header`
   display: flex;
   margin: 100px auto 56px;
   align-items: start;
